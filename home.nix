5546dc845b63dd59f347c17fc9af277b30216bdb
Andreas Enberg <andreas.fg.enberg@gmail.com>
2025-01-16 21:22:46 +0100

    Added spacer

--- home.nix
+++ home.nix
@@ -39,15 +39,12 @@ in
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    jq
-    htop
-    curl
-    nixfmt-rfc-style
     # coreutils
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
+    curl
+    htop
+    jq
+    nixfmt-rfc-style
+    spacer
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
