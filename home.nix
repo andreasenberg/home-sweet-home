01dcac768e09fedc292da696017c3550ce9c575f
Andreas Enberg <andreas.fg.enberg@gmail.com>
2025-01-17 22:33:37 +0100

    Added Hack nerd font

--- home.nix
+++ home.nix
@@ -45,12 +45,7 @@ in
     jq
     nixfmt-rfc-style
     spacer
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
+    pkgs.nerd-fonts.hack
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
