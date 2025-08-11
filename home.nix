858ad563e4c932d0ad991ec9621b30412e6e8914
Andreas Enberg <andreas.fg.enberg@gmail.com>
2025-08-11 15:59:39 +0200

    Replace deprecated jujutsu config
    This is done to resolve the following warnings:
    
    Warning: Deprecated user-level config: ui.default-description is updated to template-aliases.default_commit_description
    Warning: Deprecated user-level config: ui.diff.tool is renamed to ui.diff-formatter

--- home.nix
+++ home.nix
@@ -146,22 +146,12 @@ in
         default-command = "log";
         merge-editor = "vscode";
         conflict-marker-style = "git";
-        diff.tool = [
+        diff-formatter = [
           "difft"
           "--color=always"
           "$left"
           "$right"
         ];
-
-        default-description = ''
-          JJ: If applied, this commit will...
-
-          JJ: Explain why this change is being made
-
-          JJ: Provide links to any relevant tickets, articles or other resources
-
-          JJ: -------------------------------------------
-        '';
       };
 
       colors = {
@@ -171,6 +161,18 @@ in
       template-aliases = {
         # Set username instead of email because domain is reduntant.
         "format_short_signature(signature)" = "signature.email().local()";
+
+        default_commit_description = ''
+          "JJ: If applied, this commit will...
+
+          JJ: Explain why this change is being made
+
+          JJ: Provide links to any relevant tickets, articles or other resources
+
+          JJ: -------------------------------------------
+
+          "
+        '';
       };
     };
   };
