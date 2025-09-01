2d6667f1d2f96404b6f57bb474dc73317ceb9892
Andreas Enberg <andreas.fg.enberg@gmail.com>
2025-09-01 08:38:36 +0200

    Helix config update
    Language server config
    Changed theme to catppuccin

--- home.nix
+++ home.nix
@@ -263,9 +263,10 @@ in
     enable = true;
     defaultEditor = true;
     settings = {
-      theme = "molokai";
+      theme = "catppuccin_mocha";
       editor = {
         bufferline = "multiple";
+        cursorline = true;
         color-modes = true;
         file-picker.hidden = false;
         cursor-shape = {
@@ -276,11 +277,55 @@ in
       };
     };
     languages = {
+      language-server.pyright = {
+        command = "pyright-langserver";
+        args = [ "--stdio" ];
+        config.reportMissingtypeStubs = false;
+        config.python.analysis.typeCheckingMode = "off";
+      };
+
+      language-server.ruff = {
+        command = "ruff";
+        args = [ "server" ];
+      };
+
+      language-server.pylsp.config.pylsp.plugins = {
+        pylsp-mypy.enabled = true;
+      };
+
       language = [
         {
           name = "nix";
           auto-format = true;
           formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+        }
+        {
+          name = "python";
+          auto-format = true;
+          language-servers = [
+            {
+              name = "ruff";
+              only-features = [
+                "format"
+                "diagnostics"
+                "code-action"
+              ];
+            }
+            {
+              name = "pyright";
+              except-features = [
+                "format"
+                "diagnostics"
+              ];
+            }
+            {
+              name = "pylsp";
+              only-features = [
+                "diagnostics"
+                "code-action"
+              ];
+            }
+          ];
         }
       ];
     };
