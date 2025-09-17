6d136aed412f48ea871430a9a2f432a3150e47e5
Andreas Enberg <andreas.fg.enberg@gmail.com>
2025-09-17 14:01:35 +0200

    Activate onActivation cleanup for brew
    This is done to uninstall formulas when removed from the generated brewfile

--- darwin.nix
+++ darwin.nix
@@ -82,6 +82,9 @@
   homebrew = {
     enable = true;
 
+    # brew uninstall --zap all installed formulas not present in the generated brew file
+    onActivation.cleanup = "uninstall";
+
     casks = [
       "docker"
       "ghostty"
