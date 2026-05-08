c03ab82261f43e50dd1952e7399659fe9a8f0988
Andreas Enberg <andreas.fg.enberg@gmail.com>
2026-05-08 11:02:29 +0200

    fix(zed): Config files as symlinks
    The zed config file sources is now using mkOutOfStoreSymlink and the dotfile repoPath.
    This makes it possible to change settings within zed (as its not located in the nix store) and syncing that with the config in the repo.

--- modules/home/zed/zed.nix
+++ modules/home/zed/zed.nix
@@ -1,17 +1,17 @@
 { ... }:
 {
   flake.homeModules.zed =
-    { ... }:
+    { config, ... }:
     {
       # Installed via homebrew at the moment
       home.file = {
         ".config/zed/settings.json" = {
-          source = ./settings.json;
+          source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles.repoPath}/modules/home/zed/settings.json";
           force = true;
         };
 
         ".config/zed/keymap.json" = {
-          source = ./keymap.json;
+          source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles.repoPath}/modules/home/zed/keymap.json";
           force = true;
         };
 
